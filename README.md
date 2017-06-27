9f99390ece0f2eeabca630932d858e92785b1c1a
agusibrahim <pymobile123@gmail.com>
2017-06-27 21:38:09 +0700

    add files to stage

---
+++ README.md
@@ -0,0 +1,10 @@
+## QuickReturn ListView
+Hideable View while ListView scrolled
+
+> View yang bisa bersembunyi saat ListView di scroll
+
+Return Animation using [ViewPropertyAnimator](https://developer.android.com/reference/android/view/ViewPropertyAnimator.html) and Scroll detect direction using [View.OnTouchListener](https://developer.android.com/reference/android/view/View.OnTouchListener.html)
+
+> Animasi sembunyi/tampil menggunakan [ViewPropertyAnimator](https://developer.android.com/reference/android/view/ViewPropertyAnimator.html) dan deteksi arah scroll menggunakan [View.OnTouchListener](https://developer.android.com/reference/android/view/View.OnTouchListener.html)
+
+![image](/storage/emulated/0/recordmaster/VideoEdit/quickret.gif)
--- app/src/main/java/ai/quickreturn/MainActivity.java
+++ app/src/main/java/ai/quickreturn/MainActivity.java
@@ -2,13 +2,45 @@ package ai.quickreturn;
 
 import android.app.*;
 import android.os.*;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.view.View;
+import android.view.MotionEvent;
+import android.view.*;
 
-public class MainActivity extends Activity 
-{
+public class MainActivity extends Activity{
+	String[] data=new String[50];
+	ListView lv;
+	View ll;
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		lv=(ListView) findViewById(R.id.mainListView1);
+		ll=findViewById(R.id.mainLinearLayout1);
+		for (int i=0;i < data.length;i++) data[i]="Item " + i;
+		lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data));
+		lv.setOnTouchListener(new View.OnTouchListener() {
+			float height;
+			boolean hastouching;
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				int action = event.getAction();
+				float height = event.getY();
+				if (action == MotionEvent.ACTION_DOWN) {
+					this.height = height;
+				}
+				if (this.height < height) {
+					if (hastouching) ll.animate().setDuration(600).translationY(0);
+					hastouching = false;
+					setTitle("Show");
+				} else if (this.height > height) {
+					if (!hastouching) ll.animate().setDuration(600).translationY(500);
+					hastouching = true;
+					setTitle("Hide");
+				}
+				return false;
+			}
+		});
     }
 }
